04b65d9af48e6bed9b58124a777b1f4b0b782555
maxandbe1 <maxandbe1@proton.me>
2026-05-28 15:28:44 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { PortalShell } from "./layout/PortalShell.jsx";
 import { bootstrapPortal } from "../runtime/bootstrap.js";
-import "./styles.css";
 
 bootstrapPortal();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <PortalShell />
-  </React.StrictMode>
+  <PortalShell />
 );
